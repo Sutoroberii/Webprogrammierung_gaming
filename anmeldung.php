518d713b41179edc2ed8c2e93d32d6bb58fdb181
Sutoroberii <marenschaa@gmail.com>
2026-05-25 01:43:35 +0200

    php für anmeldung

--- anmeldung.php
+++ anmeldung.php
@@ -52,3 +52,14 @@ $title = "Anmeldung";
 
 </body>
 </html>
+
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+if(!isset($abs_path)) {
+    require_once "path.php";
+}
+
+require_once $abs_path . "anmeldung.php";
